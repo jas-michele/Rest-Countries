d18e60b536ad2de58e66c242d5ac715532f51f7d
Jasmine <jasmineoliver158@gmail.com>
2026-05-05 20:40:14 -0400

    Added useCountries custom hook

--- src/hooks/useCounties.tsx
+++ src/hooks/useCounties.tsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from "react";
 
-interface CoutriesState<T> {
+interface CountriesState<T> {
     data: T | null;
     loading: boolean;
     error: Error | null;
 }
 
- function useCountries<T>(url: string, options?: RequestInit): CoutriesState<T> {
+function useCountries<T>(url: string, options?: RequestInit): CountriesState<T> {
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<Error | null>(null);
 
-    
+    useEffect(() => {
+        const controller = new AbortController();
 
+        fetch(url, { ...options, signal: controller.signal })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Failed to fetch counties")
+                }
+                return res.json();
+            })
+            .then((data: T) => {
+                setData(data);
+                setLoading(false)
+            })
+            .catch(err => {
+                if (err.name !== "AbortError") {
+                    setError(err);
+                    setLoading(false)
+                }
+            })
 
- }
+        return () => controller.abort();
+    }, [url, options])
 
- 
+    return { data, loading, error };
+
+}
+
+export default useCountries;
